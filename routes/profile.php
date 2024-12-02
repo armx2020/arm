ea8168614123578b6b1b4f26fb4465eb47d894f0
ManasArs13 <manasyan.a.s@ya.ru>
2024-12-02 08:29:26 +0300

    Правка роутов

--- routes/profile.php
+++ routes/profile.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\GroupController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\GroupController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Profile\MyCompanyController;
