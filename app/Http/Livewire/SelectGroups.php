b3cdc5f0b919ca748e0c65559144523a83d291bc
ManasArs13 <manasyan.a.s@ya.ru>
2024-12-04 14:24:32 +0300

    Правка - поиска групп

--- app/Http/Livewire/SelectGroups.php
+++ app/Http/Livewire/SelectGroups.php
@@ -5,94 +5,57 @@ namespace App\Http\Livewire;
 use App\Models\Category;
 use App\Models\Group;
 use App\Models\Region;
-use Livewire\Component;
-use Livewire\WithPagination;
-use Illuminate\Http\Request;
 
-class SelectGroups extends Component
+class SelectGroups extends BaseSelect
 {
-    use WithPagination;
 
-    public $term = 0;
-    public $sort = "updated_at|desc";
-    public $view = 2;
-    public $region;
+    public $category = 'Все';
 
-    public function mount(Request $request, $regionCode = null)
+    public function __construct()
     {
-        if($regionCode) {
-            $reg = Region::where('code', '=', $regionCode)->First();
-        } else {
-            $reg = Region::where('name', '=', $request->session()->get('region'))->First();
-        }
-
-        if (empty($reg)) {
-            $this->region = 1;
-        } else {
-            $this->region = $reg->id;
-        }
-
-        switch ($request->session()->pull('filter')) {
-            case 'places':
-                $this->term = 6;
-                break;
-            case 'religion':
-                $this->term = 5;
-                break;
-        }
+        parent::__construct();
     }
 
     public function render()
     {
+        $entityShowRout = 'groups.show';
         $exp = explode('|', $this->sort);
 
-        if ($this->term == 0 && $this->region == 1) {
-            $groups = Group::with('users')->where('activity', 1)->orderBy($exp[0], $exp[1])->paginate(12);
-            $recommendations = [];
-        } elseif ($this->term !== 0 && $this->region == 1) {
-            $groups = Group::with('users', 'region')
-                ->where('activity', 1)
-                ->where('category_id', '=', $this->term)
-                ->orderBy($exp[0], $exp[1])
-                ->paginate(12);
-            $recommendations = [];
-        } elseif ($this->term == 0 && $this->region !== 1) {
-            $groups = Group::with('users', 'region')
-                ->where('activity', 1)
-                ->where('region_id', '=', $this->region)
-                ->orderBy($exp[0], $exp[1])
-                ->paginate(12);
+        $groups = Group::query()->with('users')->active()->orderBy($exp[0], $exp[1]);
+        $recommendations = [];
 
-            $recommendations = Group::with('user', 'region')
-                ->where('activity', 1)
+        if ($this->region !== 1) {
+            $groups = $groups
+                ->where('region_id', '=', $this->region);
+
+            $recommendations = $recommendations = Group::query()->active()
+                ->orderBy($exp[0], $exp[1])
+                ->with(['categories', 'region'])
                 ->whereNot(function ($query) {
                     $query->where('region_id', '=', $this->region);
-                })->limit(3)->get();
-        } else {
-            $groups = Group::with('users', 'region')
-                ->where('activity', 1)
-                ->where('category_id', '=', $this->term)
-                ->where('region_id', '=', $this->region)
-                ->orderBy($exp[0], $exp[1])
-                ->paginate(12);
-
-            $recommendations = Group::with('user', 'region')
-                ->where('activity', 1)
-                ->where('category_id', '=', $this->term)
-                ->whereNot(function ($query) {
-                    $query->where('region_id', '=', $this->region);
-                })->limit(3)->get();
+                })
+                ->limit(3)
+                ->get();
         }
 
-        $categories = Category::active()->main()->group()->orderBy('sort_id', 'asc')->get();
+
+        if ($this->category !== 'Все') {
+            $groups = $groups->where('category_id', '=', $this->category);
+        }
+
+        $groups = $groups->paginate(12);
+        $categories = Category::active()->main()->group()->orderBy('sort_id')->get();
+
         $regions = Region::all();
 
         return view('livewire.select-groups', [
-            'groups' => $groups,
-            'categories' => $categories,
-            'recommendations' => $recommendations,
+            'entityShowRout' => $entityShowRout,
+            'entities' => $groups,
             'regions' => $regions,
-            'region' => $this->region
+            'region' => $this->region,
+            'recommendations' => $recommendations,
+            'position' => $this->position,
+            'categories' => $categories,
         ]);
     }
 }
