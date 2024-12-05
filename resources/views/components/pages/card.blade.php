61aa241d86114eae45c22d2c3f68e083c30e10fd
ManasArs13 <manasyan.a.s@ya.ru>
2024-12-05 17:23:53 +0300

    Update card.blade.php

--- resources/views/components/pages/card.blade.php
+++ resources/views/components/pages/card.blade.php
@@ -24,14 +24,14 @@
     }
 @endphp
 
-<div class="{{ $flex }} p-2">
+<div class="{{ $flex }} p-4">
     <a href="{{ route($entityShowRout, ['id' => $entity->id]) }}" class="{{ $aClass }}">
         <img class="{{ $imageClass }} w-full rounded-2xl flex object-cover"
             src={{ isset($entity->image) ? asset('storage/' . $entity->image) : url('/image/groups.png') }}
             alt="{{ $entity->name }}" />
     </a>
     <div class="{{ $bodyClass }}">
-        <div class="flex max-h-14">
+        <div class="flex max-h-18">
             <a href="{{ route($entityShowRout, ['id' => $entity->id]) }}">
                 <p
                     class="mb-2 mt-2 sm:mt-0 break-words text-lg font-medium leading-tight text-neutral-800 text-ellipsis overflow-hidden">
@@ -44,15 +44,12 @@
         </div>
 
         @isset($entity->description)
-            <div class="flex max-h-14">
-                <p
-                    class="mb-2 break-words text-xs font-medium leading-tight text-neutral-600 line-clamp-3 text-ellipsis overflow-hidden">
-                    {{ mb_substr($entity->description, 0, 300, 'UTF-8') }}
-                    @if (mb_strlen($entity->description) > 300)
-                        ...
-                    @endif
-                </p>
-            </div>
+            <p class="mb-2 break-words text-xs font-medium text-neutral-600">
+                {{ mb_substr($entity->description, 0, 350, 'UTF-8') }}
+                @if (mb_strlen($entity->description) > 350)
+                    ...
+                @endif
+            </p>
         @endisset
 
         @if ($position == 2)
@@ -83,7 +80,7 @@
 
 </div>
 <div class="{{ $infoClass }} flex flex-col">
-<p class="text-xs mb-1">
+<p class="text-sm mb-1">
     @isset($entity->phone)
         <a href="tel:{{ $entity->phone }}" class="text-blue-600">
             {{ $entity->phone }}
