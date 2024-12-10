82fb06183ceab7246126c0e25ad1c166a1765a57
ManasArs13 <manasyan.a.s@ya.ru>
2024-12-10 08:47:00 +0300

    Правка

--- resources/views/components/pages/card.blade.php
+++ resources/views/components/pages/card.blade.php
@@ -2,25 +2,24 @@
     switch ($position) {
         case 2:
             $flex = 'flex flex-row rounded-lg bg-white h-36 sm:h-64';
-            $aClass = 'basis-1/4 flex-none';
-            $imageClass = 'h-full w-full rounded-2xl flex object-cover';
-            $bodyClass = 'px-2 lg:px-5 flex flex-col basis-3/4 sm:basis-full max-w-48 grow-0 flex-1';
-            $infoClass = 'hidden lg:flex sm:basis-1/4 flex-initial text-right';
+            $aClass = 'flex basis-1/4';
+            $imageClass = 'w-full h-full rounded-2xl flex object-cover';
+            $bodyClass = 'px-2 lg:px-5 flex flex-col max-w-48 flex-1';
+            $infoClass = 'hidden lg:flex flex-initial text-right';
             break;
     }
 @endphp
 
-<div class="{{ $flex }} p-2 sm:p-4 truncate">
+<div class="{{ $flex }} p-2 sm:p-4 truncate relative">
     <a href="{{ route($entityShowRout, ['id' => $entity->id]) }}" class="{{ $aClass }}">
-        <img class="{{ $imageClass }} w-full rounded-2xl flex object-cover"
+        <img class="{{ $imageClass }} rounded-2xl flex object-cover"
             src={{ isset($entity->image) ? asset('storage/' . $entity->image) : url('/image/groups.png') }}
             alt="{{ $entity->name }}" />
     </a>
     <div class="{{ $bodyClass }} truncate">
-        <div class="flex ">
+        <div class="flex truncate">
             <a href="{{ route($entityShowRout, ['id' => $entity->id]) }}">
-                <p
-                    class="mb-2 mt-2 sm:mt-0 text-xs md:text-base font-semibold leading-tight text-neutral-700 truncate">
+                <p class="mb-2 mt-2 sm:mt-0 text-xs md:text-base font-semibold leading-tight text-neutral-700 truncate">
                     {{ mb_substr($entity->name, 0, 1300, 'UTF-8') }}
                     @if (mb_strlen($entity->name) > 1300)
                         ...
@@ -65,10 +64,12 @@
 @endif
 
 @if ($entity->city_id && $entity->city_id !== 1)
-    <p class="mt-3 break-words text-xs lg:text-sm font-semibold text-blue-500">
+    <p class="mt-3 break-words text-xs lg:text-sm font-semibold text-blue-500 absolute top-24 sm:top-48">
         {{ $entity->city->name }}
     </p>
 @endif
+
+
 
 </div>
 <div class="{{ $infoClass }} flex flex-col">
