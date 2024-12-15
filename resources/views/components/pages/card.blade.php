05a160821f2cfe701c1a5720c554aa191cfbe994
ManasArs13 <manasyan.a.s@ya.ru>
2024-12-15 17:05:45 +0300

    Update card.blade.php

--- resources/views/components/pages/card.blade.php
+++ resources/views/components/pages/card.blade.php
@@ -1,13 +1,13 @@
-<div class="flex flex-row rounded-lg bg-white min-h-36 sm:min-h-64 p-2 sm:p-4 truncate relative">
+<div class="flex flex-row rounded-lg bg-white h-36 md:h-64 p-2 sm:p-4 truncate relative">
     <a href="{{ route($entityShowRout, ['id' => $entity->id]) }}">
-        <img class="h-24 w-24 md:h-40 md:w-40 lg:h-48 lg:w-48 rounded-2xl object-cover rounded-2xl object-cover"
+        <img class="h-32 w-32 md:h-56 md:w-56 rounded-lg object-cover"
             src={{ isset($entity->image) ? asset('storage/' . $entity->image) : url('/image/groups.png') }}
             alt="{{ $entity->name }}" />
     </a>
-    <div class="px-2 lg:px-5 flex flex-col max-w-48 flex-1 truncate">
+    <div class="px-3 lg:px-5 flex flex-col max-w-48 flex-1 truncate">
         <div class="flex truncate">
             <a href="{{ route($entityShowRout, ['id' => $entity->id]) }}">
-                <p class="mb-2 mt-2 sm:mt-0 text-xs md:text-base font-semibold leading-tight text-neutral-700 truncate">
+                <p class="mb-2 mt-2 sm:mt-0 text-xs md:text-base lg:text-xl font-bold leading-tight text-neutral-700 truncate">
                     {{ mb_substr($entity->name, 0, 1300, 'UTF-8') }}
                     @if (mb_strlen($entity->name) > 1300)
                         ...
@@ -41,8 +41,7 @@
 @endif
 
 @if ($entity->city_id && $entity->city_id !== 1)
-    <p
-        class="mt-3 break-words text-xs lg:text-sm font-semibold text-blue-500 absolute top-24 sm:top-48 block lg:hidden">
+    <p class="mt-3 break-words text-xs font-medium text-blue-500 absolute top-24 sm:top-48 block lg:hidden">
         {{ $entity->city->name }}
     </p>
 @endif
@@ -54,7 +53,7 @@
 
 </div>
 <div class="hidden lg:flex flex-initial text-right flex flex-col">
-<p class="text-sm mb-1">
+<p class="text-lg mb-1 font-medium">
     @isset($entity->phone)
         <a href="tel:{{ $entity->phone }}" class="text-blue-600">
             {{ $entity->phone }}
@@ -62,7 +61,7 @@
     @endisset
 
     @if ($entity->city_id && $entity->city_id !== 1)
-        <p class="break-words text-xs font-semibold text-blue-500 hidden lg:block">
+        <p class="break-words text-lg font-medium text-blue-500 hidden lg:block">
             {{ $entity->city->name }}
         </p>
     @endif
