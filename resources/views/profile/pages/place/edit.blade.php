3689587063c2f5384f45bf0714b8cfc8492fe1e8
ManasArs13 <manasyan.a.s@ya.ru>
2025-01-27 10:01:52 +0300

    Update edit.blade.php

--- resources/views/profile/pages/place/edit.blade.php
+++ resources/views/profile/pages/place/edit.blade.php
@@ -212,7 +212,7 @@
                                             <div class="flex">
                                                 @if (count($item->categories) < 1)
                                                     <input type="radio" name="category" value="{{ $item->id }}"
-                                                        @checked($offer->category_id == $item->id)
+                                                        @checked($entity->category_id == $item->id)
                                                         @if (is_array(old('category')) && in_array($item->id, old('category'))) checked @endif
                                                         class="checkbox-{{ $loop->iteration }} shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
                                                         id="checkbox-{{ $item->id }}">
