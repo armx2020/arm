8f7e620bac058cd42a9ee6b5b9dd3e015af57093
ManasArs13 <manasyan.a.s@ya.ru>
2025-02-04 14:53:38 +0300

    Правки

--- resources/views/pages/entity/index.blade.php
+++ resources/views/pages/entity/index.blade.php
@@ -3,18 +3,24 @@
 @php
     $sitemap = App\Models\SiteMap::where('url', url()->current())->First();
 
-    $title = $sitemap->title;
-    $description = $sitemap->description;
+    $title = 'ВСЕ АРМЯНЕ';
+    $description = 'Сообщество армян в России';
+
+    if ($sitemap) {
+        $title = $sitemap->title;
+        $description = $sitemap->description;
+    }
+
 @endphp
 
 @section('title')
-    <title>{{ $title ?: 'ВСЕ АРМЯНЕ' }}
+    <title>{{ $title }}
     </title>
 @endsection
 
 @section('meta')
     <meta name="robots" content="index, follow" />
-    <meta name="description" content={{ $description ?: 'Сообщество армян в России' }}>
+    <meta name="description" content={{ $description }}>
 @endsection
 
 @section('scripts')
