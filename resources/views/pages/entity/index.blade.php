e7b49f4eeb5e006224fd64be50a442e184b04fe7
ManasArs13 <manasyan.a.s@ya.ru>
2025-02-04 14:47:10 +0300

    Правки

--- resources/views/pages/entity/index.blade.php
+++ resources/views/pages/entity/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('title')
-    <title>{{ App\Models\SiteMap::select('url')->where('url', url()->current())->first()?->title }}
+    <title>{{ App\Models\SiteMap::where('url', url()->current())->First() ?->title ?: 'ВСЕ АРМЯНЕ' }}
     </title>
 @endsection
 
 @section('meta')
     <meta name="robots" content="index, follow" />
     <meta name="description"
-        content="{{ App\Models\SiteMap::select('url')->where('url', url()->current())->first()?->description }}">
+        content={{ App\Models\SiteMap::where('url', url()->current())->First() ?->description ?: 'Сообщество армян в России' }}>
 @endsection
 
 @section('scripts')
