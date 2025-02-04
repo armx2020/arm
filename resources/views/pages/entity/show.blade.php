e157d59ee2540b12fbf6de0342a76b1f32ed1b76
ManasArs13 <manasyan.a.s@ya.ru>
2025-02-04 14:50:58 +0300

    Правки

--- resources/views/pages/entity/show.blade.php
+++ resources/views/pages/entity/show.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.app')
 
+@php
+    $sitemap = App\Models\SiteMap::where('url', url()->current())->First();
+
+    $title = $sitemap->title;
+    $description = $sitemap->description;
+@endphp
+
 @section('title')
-    <title>{{ App\Models\SiteMap::select('url')->where('url', url()->current())->first()?->title }}
+    <title>{{ $title ?: 'ВСЕ АРМЯНЕ' }}
     </title>
 @endsection
 
 @section('meta')
     <meta name="robots" content="index, follow" />
-    <meta name="description"
-        content="{{ App\Models\SiteMap::select('url')->where('url', url()->current())->first()?->description }}">
+    <meta name="description" content={{ $description ?: 'Сообщество армян в России' }}>
 @endsection
 
 @section('content')
