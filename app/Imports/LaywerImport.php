ea28436bbc4c649c50b1ef613711c60d4be1a1c9
ManasArs13 <manasyan.a.s@ya.ru>
2025-02-18 13:29:44 +0300

    Update LaywerImport.php

--- app/Imports/LaywerImport.php
+++ app/Imports/LaywerImport.php
@@ -2,80 +2,90 @@
 
 namespace App\Imports;
 
+use App\Models\Category;
 use App\Models\City;
 use App\Models\Entity;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\PersistRelations;
+use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 use Maatwebsite\Excel\Concerns\WithUpserts;
 
-class LaywerImport implements ToCollection, WithUpserts, PersistRelations, WithStartRow
+class LaywerImport implements ToCollection, WithUpserts, PersistRelations, WithStartRow, WithBatchInserts
 {
+    public $comment = '';
+    public $result = [];
 
     public function collection(Collection $rows)
     {
         foreach ($rows as $row) {
+
+            $this->getCategories([$row[12], $row[13], $row[14], $row[15], $row[16], $row[17], $row[18], $row[18]]);
+
             $entity = Entity::updateOrCreate(
                 [
                     'name' => $row[2]
                 ],
                 [
-                    'link' => $row[1] == '_' ? null : $row[1],
-                    'description' => $row[3] == '_' ? null : $row[3],
-                    'city_id' => $this->getCityName($row[4] == '_' ? null : $row[4]),
-                    'region_id' => $this->getRegionName($row[4] == '_' ? null : $row[4]),
-                    'address' => $this->getAddress($row[5]),
-                    'phone' => $row[6] == '_' ? null : $row[6],
-                    'whatsapp' => $row[7] == '_' ? null : $row[7],
-                    'email' =>  $row[8] == '_' ? null : $row[8],
-                    'web' => $row[9] == '_' ? null : $row[9],
-                    'telegram' => $row[10] == '_' ? null : $row[10],
-                    'vkontakte' => $row[11] == '_' ? null : $row[11],
+                    // 'link' => $row[1] == '_' ? null : $row[1],
+                    // 'description' => $row[3] == '_' ? null : $row[3],
+                    // 'city_id' => $this->getCityName($row[4] == '_' ? null : $row[4]),
+                    // 'region_id' => $this->getRegionName($row[4] == '_' ? null : $row[4]),
+                    // 'address' => $this->getAddress($row[5]),
+                    // 'phone' => $row[6] == '_' ? null : $row[6],
+                    // 'whatsapp' => $row[7] == '_' ? null : $row[7],
+                    // 'email' =>  $row[8] == '_' ? null : $row[8],
+                    // 'web' => $row[9] == '_' ? null : $row[9],
+                    // 'telegram' => $row[10] == '_' ? null : $row[10],
+                    // 'vkontakte' => $row[11] == '_' ? null : $row[11],
 
-                    'comment' => $this->getComment([$row[12], $row[13], $row[14], $row[15], $row[16], $row[17], $row[18], $row[18]]),
+                    'comment' => $this->comment,
                     'entity_type_id' => 1,
                     'category_id' => 78,
 
-                    'image' => null,
+                    //    'image' => null,
                     // 'activity' => false,
                 ]
             );
 
-           $entity->fields()->syncWithPivotValues([86, 87, 88, 91], ['main_category_id' => 78]);
+            $entity->fields()->syncWithPivotValues($this->result, ['main_category_id' => 78]);
+
+            $this->comment = '';
+            $this->result = [];
 
 
-            if (Storage::disk('public')->exists("uploaded/lawyer/$row[0]/1.png")) {
-                $entity->image = "uploaded/lawyer/$row[0]/1.png";
-                $entity->update();
-            }
+            // if (Storage::disk('public')->exists("uploaded/lawyer/$row[0]/1.png")) {
+            //     $entity->image = "uploaded/lawyer/$row[0]/1.png";
+            //     $entity->update();
+            // }
 
-            $entity->images()->delete();
+            //   $entity->images()->delete();
 
-            if (Storage::disk('public')->exists("uploaded/lawyer/$row[0]/2.png")) {
-                $entity->images()->create([
-                    'path' => "uploaded/lawyer/$row[0]/2.png"
-                ]);
-            }
+            // if (Storage::disk('public')->exists("uploaded/lawyer/$row[0]/2.png")) {
+            //     $entity->images()->create([
+            //         'path' => "uploaded/lawyer/$row[0]/2.png"
+            //     ]);
+            // }
 
-            if (Storage::disk('public')->exists("uploaded/lawyer/$row[0]/3.png")) {
-                $entity->images()->create([
-                    'path' => "uploaded/lawyer/$row[0]/3.png"
-                ]);
-            }
+            // if (Storage::disk('public')->exists("uploaded/lawyer/$row[0]/3.png")) {
+            //     $entity->images()->create([
+            //         'path' => "uploaded/lawyer/$row[0]/3.png"
+            //     ]);
+            // }
 
-            if (Storage::disk('public')->exists("uploaded/lawyer/$row[0]/4.png")) {
-                $entity->images()->create([
-                    'path' => "uploaded/lawyer/$row[0]/4.png"
-                ]);
-            }
+            // if (Storage::disk('public')->exists("uploaded/lawyer/$row[0]/4.png")) {
+            //     $entity->images()->create([
+            //         'path' => "uploaded/lawyer/$row[0]/4.png"
+            //     ]);
+            // }
 
-            if (Storage::disk('public')->exists("uploaded/lawyer/$row[0]/5.png")) {
-                $entity->images()->create([
-                    'path' => "uploaded/lawyer/$row[0]/5.png"
-                ]);
-            }
+            // if (Storage::disk('public')->exists("uploaded/lawyer/$row[0]/5.png")) {
+            //     $entity->images()->create([
+            //         'path' => "uploaded/lawyer/$row[0]/5.png"
+            //     ]);
+            // }
         }
     }
 
@@ -87,6 +97,11 @@ class LaywerImport implements ToCollection, WithUpserts, PersistRelations, WithS
     public function uniqueBy()
     {
         return 'name';
+    }
+
+    public function batchSize(): int
+    {
+        return 200;
     }
 
     public function getCityName($data)
@@ -125,5 +140,38 @@ class LaywerImport implements ToCollection, WithUpserts, PersistRelations, WithS
         }
 
         return $address;
+    }
+
+    public function getCategories($data = [])
+    {
+        foreach ($data as $row) {
+
+            $isFind = false;
+
+            if (isset($row)) {
+                $words = explode(" ", $row);
+
+                for ($i = 0, $k = 1; $i < count($words); $i++, $k++) {
+                    $category = Category::where('name', 'LIKE', "%$words[$i]%")->First();
+
+                    if (!$category && isset($words[$k])) {
+                        $category = Category::where('name', 'LIKE', "%$words[$i]" . ' ' . "$words[$k]%")->First();
+                    }
+
+                    if ($category) {
+                        $this->result[] = $category->category_id ?:  $category->id;
+                        $isFind = true;
+                    }
+                }
+
+                if(!$isFind) {
+                    if($this->comment == '') {
+                        $this->comment = "Не найденные строки: " .  $row. "; ";
+                    } else {
+                        $this->comment = $this->comment .  $row. "; ";
+                    }
+                }
+            }
+        }
     }
 }
