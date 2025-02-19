f241b1fa8bd6e399292ee33f7d4de5bb47f9960a
ManasArs13 <manasyan.a.s@ya.ru>
2025-02-19 11:13:28 +0300

    Update sub-category-select.blade.php

--- resources/views/components/pages/sub-category-select.blade.php
+++ resources/views/components/pages/sub-category-select.blade.php
@@ -4,7 +4,7 @@
     <div class="flex flex-row gap-3">
 
         <div class="bg-white mt-3 basis-full rounded-md p-3 hidden lg:block">
-            <h4 class="mb-2 font-semibold text-gray-900 dark:text-white">Подкатегория</h4>
+            <h4 class="mb-2 font-semibold text-gray-900 dark:text-white">Специализация</h4>
             <div class="flex items-center mb-2">
                 <input type="radio" value="Все" id="all_categories" wire:model.live="subCategory"
                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
@@ -27,7 +27,7 @@
             <div class="bg-white mt-3 basis-full rounded-md block lg:hidden">
                 <select name="subCategory" class="w-full border-0 rounded-md" wire:model.live="subCategory"
                     autocomplete="off">
-                    <option value='Все'>Все категории</option>
+                    <option value='Все'>Все специализации</option>
                     @foreach ($categories as $category)
                         <option value='{{ $category->id }}'>{{ $category->name }}</option>
                     @endforeach
