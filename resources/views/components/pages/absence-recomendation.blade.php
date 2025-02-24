4e3e82b80e52deb171bc1f1548e8cad504143b57
ManasArs13 <manasyan.a.s@ya.ru>
2025-02-24 09:31:56 +0300

    Добавлена плашка -показ из других регионов

---
+++ resources/views/components/pages/absence-recomendation.blade.php
@@ -0,0 +1,6 @@
+<div class="w-full text-center">
+    <div class="flex basis-full bg-green-100 rounded-lg px-6 py-2 text-base text-green-700"
+        role="alert" style="max-height:64px;">
+        К сожалению, в этой области нет подходящих вариантов, посмотрите примеры из других областей
+    </div>
+</div>
--- resources/views/components/pages/grid.blade.php
+++ resources/views/components/pages/grid.blade.php
@@ -1,5 +1,24 @@
 <div class="grid grid-cols-1 gap-3 lg:gap-5">
+
+    @php
+        $isNeedRecomendation = true;
+    @endphp
+
     @foreach ($entities as $entity)
+        @if ($isNeedRecomendation && "$entity->region_id" !== $this->region)
+            @if ($loop->iteration == 1)
+                <x-pages.absence-recomendation />
+            @else
+                <div class="mt-8">
+                    <x-pages.absence-recomendation />
+                </div>
+            @endif
+
+
+            @php
+                $isNeedRecomendation = false;
+            @endphp
+        @endif
         <x-pages.card :entity="$entity" :$entityShowRout />
     @endforeach
 </div>
