794af139c6f6599a574bb2c1a83199471091886d
ManasArs13 <manasyan.a.s@ya.ru>
2025-03-06 12:20:22 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,7 +75,7 @@
                         <a
                             @if (isset($regionName) && $regionName !== 'Россия') href="{{ route('companies.region', ['regionTranslit' => $region]) }}" @else  href="{{ route('companies.index') }}" @endif>
                             <div class="flex flex-col h-full w-full relative">
-                                <p class="mx-3 lg:m-2 text-xs md:text-lg font-bold" style="line-height:1.25rem;">Справочник услуг</p>
+                                <p class="mx-3 lg:m-2 text-xs md:text-lg font-bold w-2/3 lg:w-full" style="line-height:1.25rem;">Справочник услуг</p>
                                 <p class="mx-1 lg:mx-2 text-xs md:text-base font-light hidden lg:block">Каталог товаров/услуг</p>
                                 <div class="absolute bottom-0 right-0 w-14 md:w-2/3 lg:w-full">
                                     <img class="mb-1 flex self-center m-auto rounded-xl xl:w-[90%] lg:w-[70%] md:w-[84%] sm:w-[95%] ls:w-[75%] ms:w-[95%] es:w-[100%]"
@@ -92,7 +92,7 @@
                         <a
                             @if (isset($regionName) && $regionName !== 'Россия') href="{{ route('communities.region', ['regionTranslit' => $region]) }}" @else  href="{{ route('communities.index') }}" @endif>
                             <div class="flex flex-col h-full w-full relative">
-                                <p class="mx-3 lg:m-2 text-xs md:text-base font-bold">Армянские общины</p>
+                                <p class="mx-3 lg:m-2 text-xs md:text-base font-bold w-2/3 lg:w-full">Армянские общины</p>
                                 <p class="mx-1 lg:mx-2 text-xs md:text-base font-light hidden lg:block">Каталог товаров/услуг </p>
                                 <div class="absolute bottom-0 right-0 w-16 md:w-2/3 lg:w-full">
                                     <img class="mb-1 flex self-center m-auto rounded-xl xl:w-[90%] lg:w-[70%] md:w-[75%] sm:w-[84%] ls:w-[75%] ms:w-[90%] es:w-[100%]"
@@ -111,7 +111,7 @@
                             <div class="flex flex-col h-full w-full relative">
                                 <p class="mx-3 lg:m-2 text-xs md:text-lg font-bold w-1/2 lg:w-fukk">Поиск работы</p>
                                 <p class="mx-1 lg:mx-2 text-xs md:text-base font-light hidden lg:block">Найти работу или разместить вакансию</p>
-                                <div class="absolute bottom-0 right-1 md:right-5 w-14 md:w-1/2 lg:w-24">
+                                <div class="absolute bottom-0 right-1 md:right-8 w-14 md:w-1/2 lg:w-28">
                                     <img class="mb-1 flex self-center m-auto rounded-xl xl:w-[90%] lg:w-[70%] md:w-[84%] sm:w-[100%] ls:w-[93%] ms:w-[95%] es:w-[100%]"
                                          src="{{ url('/image/employment.png') }}" alt="banner">
                                 </div>
@@ -126,7 +126,7 @@
                         <a
                             @if (isset($regionName) && $regionName !== 'Россия') href="{{ route('projects.region', ['regionTranslit' => $region]) }}" @else  href="{{ route('projects.index') }}" @endif>
                             <div class="flex flex-col h-full w-full relative">
-                                <p class="mx-3 lg:m-2 text-xs md:text-lg font-bold">Проекты помощи</p>
+                                <p class="mx-3 lg:m-2 text-xs md:text-lg font-bold w-2/3 lg:w-full">Проекты помощи</p>
                                 <p class="mx-1 lg:mx-2 text-xs md:text-base font-light hidden lg:block">Помощь нуждающимся</p>
                                 <div class="absolute bottom-0 right-0 w-16 md:w-2/3 lg:w-full">
                                     <img class="mb-1 flex self-center m-auto rounded-xl xl:w-[90%] lg:w-[70%] md:w-[80%] sm:w-[87%] ls:w-[75%] ms:w-[95%] es:w-[100%]"
@@ -139,14 +139,14 @@
 
                 <!-- Интересные места -->
                 <div class="CEB__wrapTable mb-5 w-1/2 lg:w-1/3 xl:w-1/6 pr-1 sm:pr-1 sm:pl-1 md:pl-1.5 md:pr-1.5 xl:pr-2 xl:pl-0">
-                    <div class="bg-[#F0E7CE] rounded-xl p-2 lg:p-4 h-56 es:h-[190px] md:h-[250px] lg:h-72">
+                    <div class="bg-[#F0E7CE] rounded-xl p-2 lg:p-4 h-56 es:h-[200px] md:h-[250px] lg:h-72">
                         <a
                             @if (isset($regionName) && $regionName !== 'Россия') href="{{ route('places.region', ['regionTranslit' => $region]) }}" @else  href="{{ route('places.index') }}" @endif>
                             <div class="flex flex-col h-full w-full relative">
                                 <p class="m-1 lg:m-2 text-xs md:text-lg font-bold" style="line-height:1.25rem;">Интересные места, Церкви</p>
                                 <p class="mx-1 lg:mx-2 text-xs md:text-base font-light">Интересные места</p>
                                 <div class="absolute bottom-0 w-full">
-                                    <img class="mb-2 flex self-center m-auto rounded-xl xl:w-[90%] lg:w-[70%] md:w-[72%] sm:w-[77%] ls:w-[75%] ms:w-[87%] es:w-[100%]"
+                                    <img class="mb-2 flex self-center m-auto rounded-xl xl:w-[90%] lg:w-[70%] md:w-[72%] sm:w-[77%] ls:w-[75%] ms:w-[87%] es:w-[90%]"
                                         src="{{ url('/image/church.png') }}" alt="banner">
                                 </div>
                             </div>
@@ -156,14 +156,14 @@
 
                 <!-- Кружки сообщества -->
                 <div class="CEB__wrapTable mb-5 w-1/2 lg:w-1/3 xl:w-1/6 pl-1 xl:pl-0">
-                    <div class="bg-[#D7E6D8] rounded-xl p-2 lg:p-4 h-56 es:h-[190px] md:h-[250px] lg:h-72">
+                    <div class="bg-[#D7E6D8] rounded-xl p-2 lg:p-4 h-56 es:h-[200px] md:h-[250px] lg:h-72">
                         <a
                             @if (isset($regionName) && $regionName !== 'Россия') href="{{ route('groups.region', ['regionTranslit' => $region]) }}" @else  href="{{ route('groups.index') }}" @endif>
                             <div class="flex flex-col h-full w-full relative">
                                 <p class="m-1 lg:m-2 text-xs md:text-lg font-bold" style="line-height:1.25rem;">Кружки, сообщества</p>
                                 <p class="mx-1 lg:mx-2 text-xs md:text-base font-light">Сообщества, группы по интересам</p>
                                 <div class="absolute bottom-0 w-full">
-                                    <img class="mb-2 flex self-center m-auto rounded-xl xl:w-[90%] lg:w-[70%] md:w-[95%] sm:w-[100%] ls:w-[95%] ms:w-[100%] es:w-[100%]"
+                                    <img class="mb-2 flex self-center m-auto rounded-xl xl:w-[90%] lg:w-[70%] md:w-[95%] sm:w-[100%] ls:w-[95%] ms:w-[100%] es:w-[90%]"
                                         src="{{ url('/image/group-of-young-people-waving-hand.png') }}" alt="friends">
                                 </div>
                             </div>
