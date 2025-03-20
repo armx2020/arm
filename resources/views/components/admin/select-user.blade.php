05b5fe26d2360cacefa6032dfde30477efde7060
ManasArs13 <manasyan.a.s@ya.ru>
2025-03-20 21:35:13 +0300

    Update select-user.blade.php

--- resources/views/components/admin/select-user.blade.php
+++ resources/views/components/admin/select-user.blade.php
@@ -1,10 +1,22 @@
 @props(['selectedUser' => null])
 
-<div class="col-span-6" id="user_div"  wire:ignore>
-    <label for="user" class="text-sm font-medium text-gray-900 block mb-2">Пользователь</label>
+<div class="col-span-6" id="user_div" wire:ignore>
+    <div class="flex justify-between">
+        <label for="user" class="text-sm font-medium text-gray-900 mb-2">Пользователь</label>
+        @if (isset($selectedUser))
+            <a id="userLink" href="{{ route('admin.user.edit', ['user' => $selectedUser->id]) }}"
+                class="underline underline-offset-2 text-blue-600 hover:text-blue-500" target="blank">для
+                перехода в карточку пользователя, нажмите сюда</a>
+        @else
+            <a id="userLink" href="#"
+                class="underline underline-offset-2 text-blue-600 hover:text-blue-500 hidden" target="blank">для
+                перехода в карточку пользователя, нажмите сюда</a>
+        @endif
+    </div>
     <select name="user" class="w-full" id="user">
         @if (isset($selectedUser))
-            <option value="{{ $selectedUser->id }}"> {{ $selectedUser->firstname }} {{ $selectedUser->phone }}</option>
+            <option value="{{ $selectedUser->id }}" data-url="{{ route('admin.user.edit', ['user' => 1]) }}">
+                {{ $selectedUser->firstname }} {{ $selectedUser->phone }}</option>
         @else
             <option value=''>ИМЯ ТЕЛЕФОН ПАРОЛЬ(если не ввести, то паролем будет ИМЯ ПОЛЬЗОВАТЕЛЯ)</option>
         @endif
@@ -12,33 +24,64 @@
 </div>
 
 <script type="text/javascript">
-    $("#user").select2({
-        tags: true,
-        ajax: {
-            url: " {{ route('admin.get-user') }}",
-            type: "get",
-            delay: 250,
-            dataType: 'json',
+    $(document).ready(function() {
+        $("#user").select2({
+            tags: true,
+            ajax: {
+                url: "{{ route('admin.get-user') }}",
+                type: "get",
+                delay: 250,
+                dataType: 'json',
 
-            data: function(params) {
-                var query = {
-                    query: params.term || '',
-                    page: params.page || 1,
-                    "_token": "{{ csrf_token() }}",
-                };
+                data: function(params) {
+                    var query = {
+                        query: params.term || '',
+                        page: params.page || 1,
+                        "_token": "{{ csrf_token() }}",
+                    };
 
-                return query;
+                    return query;
+                },
+                processResults: function(response, params) {
+                    params.page = params.page || 1;
+                    return {
+                        results: response.results,
+                        pagination: {
+                            more: response.pagination.more
+                        }
+                    };
+                },
+                cache: true
             },
-            processResults: function(response, params) {
-                params.page = params.page || 1;
-                return {
-                    results: response.results,
-                    pagination: {
-                        more: response.pagination.more
-                    }
-                };
-            },
-            cache: true
+        });
+
+        $('#user').on('change', function() {
+
+            const selectedOption = $(this).val();
+
+            if (!isNaN(selectedOption)) {
+                const userLink = $('#userLink');
+
+                if (selectedOption) {
+                    userLink.attr('href', 'https://vsearmyane.ru/admin/user/' + selectedOption +
+                        '/edit');
+                    userLink.show();
+                } else {
+                    userLink.hide();
+                }
+            }
+
+        });
+
+        const selectedOption = $('#user').val();
+
+        const userLink = $('#userLink');
+
+        if (selectedOption) {
+            userLink.attr('href', 'https://vsearmyane.ru/admin/user/' + selectedOption + '/edit');
+            userLink.show(); 
+        } else {
+            userLink.hide();
         }
     });
 </script>
