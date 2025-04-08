b03114c1dde230738c9efbd589cd41263092ea61
ManasArs13 <manasyan.a.s@ya.ru>
2025-04-08 09:30:23 +0300

    Update EntityObserver.php

--- app/Observers/EntityObserver.php
+++ app/Observers/EntityObserver.php
@@ -4,32 +4,45 @@ namespace App\Observers;
 
 use App\Models\Entity;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class EntityObserver
 {
     public function saving(Entity $entity)
     {
-        if ($entity->isDirty('address') && !$entity->coordinates && $entity->address && $entity->city_id) {
+        if (!$entity->coordinates && $entity->address && $entity->city_id) {
             $this->geocode($entity);
         }
     }
 
     protected function geocode(Entity $entity)
     {
-        $response = Http::get('https://geocode-maps.yandex.ru/1.x/', [
-            'apikey' => env('YANDEX_GEOCODER_KEY'),
-            'geocode' => $entity->city->name . ', ' .$entity->address,
-            'format' => 'json',
-        ]);
+        try {
+            $response = Http::get('https://geocode-maps.yandex.ru/1.x/', [
+                'apikey' => env('YANDEX_GEOCODER_KEY'),
+                'geocode' => $entity->city->name . ', ' . $entity->address,
+                'format' => 'json',
+            ]);
 
-        $data = $response->json();
+            $data = $response->json();
 
-        if (isset($data['response']['GeoObjectCollection']['featureMember'][0])) {
-            $coords = explode(' ', 
-                $data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
-            );
-            $entity->lon = $coords[0]; // Долгота
-            $entity->lat = $coords[1]; // Широта
+            if (isset($data['response']['GeoObjectCollection']['featureMember'][0])) {
+                $coords = explode(
+                    ' ',
+                    $data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+                );
+                $entity->lon = $coords[0]; // Долгота
+                $entity->lat = $coords[1]; // Широта
+
+            } else {
+                Log::warning("No geocoding results for Entity ID: {$entity->id}", [
+                    'city' => $entity->city->name,
+                    'address' => $entity->address,
+                    'full_response' => $data
+                ]);
+            }
+        } catch (\Exception $e) {
+            Log::error("Geocoding failed for Entity ID: {$entity->id}", ['error' => $e->getMessage()]);
         }
     }
 }
