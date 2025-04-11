a3bd1c854424de90cb5d69bfdc733dc82ae17dc9
ManasArs13 <manasyan.a.s@ya.ru>
2025-04-11 10:47:07 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,18 +18,18 @@ class Kernel extends ConsoleKernel
         $schedule->command('cache-regions')->dailyAt('02:10');
         $schedule->command('cache-options')->dailyAt('02:20');
         $schedule->command('app:create-site-map --create --truncate')->dailyAt('03:00');
-        $schedule->command('cities:update-coordinates')->hourly();
+       // $schedule->command('cities:update-coordinates')->hourly();
 
         $schedule->call(function () {
             Cache::forget('yandex_geocoder_used_requests');
         })->dailyAt('00:00'); // Ежедневный сброс счётчика лимита Яндекс HTTP API
 
         // Обработка очереди каждую минуту
-        $schedule->command('queue:work --stop-when-empty --max-time=60')->dailyAt('01:00');
+       // $schedule->command('queue:work --stop-when-empty --max-time=60')->dailyAt('01:00');
 
         // Очистка завершенных задач 
-        $schedule->command('queue:prune-failed')->daily();
-        $schedule->command('queue:prune-batches')->daily();
+       // $schedule->command('queue:prune-failed')->daily();
+       // $schedule->command('queue:prune-batches')->daily();
     }
 
     protected function commands(): void
