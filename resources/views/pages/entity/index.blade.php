b9bf80195dc216025254af52e74ff669fc10c07e
ManasArs13 <manasyan.a.s@ya.ru>
2025-04-13 10:18:04 +0300

    Update index.blade.php

--- resources/views/pages/entity/index.blade.php
+++ resources/views/pages/entity/index.blade.php
@@ -332,7 +332,7 @@
 
                 <div class="flex flex-col basis-full lg:basis-4/5 lg:m-3 my-5 lg:ml-5 min-h-screen max-w-5xl">
 
-                    @if ($entityName == 'Интересные места, церкви' && isset($lat) && iseet($lon))
+                    @if ($entityName == 'Интересные места, церкви' && isset($lat) && isset($lon))
                         <x-pages.map :$lat :$lon />
                     @endif
 
